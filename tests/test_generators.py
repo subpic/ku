2ecce37deb76036e7eaf3fb31dc88f257604b9fd
Vlad Hosu <subpic@gmail.com>
2019-11-23 13:56:15 +0000

    switched to pytest

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,39 +1,34 @@
 from __future__ import print_function
 from __future__ import absolute_import
-import unittest
-
 from ku import generators as gr
 from ku import generic as gen
 from munch import Munch
 import pandas as pd, numpy as np
+import pytest
 
 gen_params = Munch(batch_size    = 2,
                    data_path     = 'images',
                    input_shape   = (224,224,3),
                    inputs        = ['filename'],
                    outputs       = ['score'],
+                   shuffle       = False,
                    fixed_batches = True)
 
 ids = pd.read_csv('ids.csv', encoding='latin-1')
 
-class TestDataGeneratorDisk(unittest.TestCase):
-    
-    def test_correct_df_input(self):
-        self.assertTrue(np.all(ids.columns == ['filename', 'score']))
-        self.assertTrue(np.all(ids.score == range(1,5)))
-        
-    def test_init_generator(self):          
-        g = gr.DataGeneratorDisk(ids, shuffle=False, **gen_params)
-        self.assertIsInstance(g[0], tuple)
-        self.assertIsInstance(g[0][0], list)
-        self.assertIsInstance(g[0][1], list)
-        self.assertEqual(gen.get_sizes(g[0]),'([array<2,224,224,3>], [array<2,1>])')
-        self.assertTrue(np.all(g[0][1][0] == np.array([[1],[2]])))
-        
-    def test_get_sizes(self):
-        x = np.array([[1,2,3]])
-        self.assertEqual(gen.get_sizes(([x.T],1,[4,5])), '([array<3,1>], <1>, [<1>, <1>])')
-        self.assertEqual(gen.get_sizes(np.array([[1,[1,2]]])), 'array<1,2>')
-    
-if __name__ == '__main__':
-    unittest.main()
+def test_correct_df_input():
+    assert (np.all(ids.columns == ['filename', 'score']))
+    assert (np.all(ids.score == range(1,5)))
+
+def test_init_generator():          
+    g = gr.DataGeneratorDisk(ids, **gen_params)
+    assert isinstance(g[0], tuple)
+    assert isinstance(g[0][0], list)
+    assert isinstance(g[0][1], list)
+    assert (gen.get_sizes(g[0]) == '([array<2,224,224,3>], [array<2,1>])')
+    assert (np.all(g[0][1][0] == np.array([[1],[2]])))
+
+def test_get_sizes():
+    x = np.array([[1,2,3]])
+    assert gen.get_sizes(([x.T],1,[4,5])) == '([array<3,1>], <1>, [<1>, <1>])'
+    assert gen.get_sizes(np.array([[1,[1,2]]])) == 'array<1,2>'
