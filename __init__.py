ace7213ab04f116d0155c3a205193adcefd0b2b6
Vlad Hosu <subpic@gmail.com>
2019-12-15 19:10:44 +0000

    remove warnings

--- __init__.py
+++ __init__.py
@@ -11,8 +11,9 @@ import logging
 class WarningFilter(logging.Filter):
     def filter(self, record):
         msg = record.getMessage()
-        tf_warning = 'retry (from tensorflow.contrib.learn.python.learn.datasets.base)' in msg
-        return not tf_warning           
+        tf_warning1 = 'retry (from tensorflow.contrib.learn.python.learn.datasets.base)' in msg        
+        tf_warning2 = 'is deprecated' in msg
+        return not (tf_warning1 or tf_warning2)
 logger = logging.getLogger('tensorflow')
 logger.addFilter(WarningFilter())
 
