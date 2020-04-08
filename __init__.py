0f1214811b23992f043c7760306794ad3371886d
Vlad Hosu <subpic@gmail.com>
2020-04-08 09:32:58 +0000

    ignore FutureWarnings

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 from __future__ import absolute_import
-from . import generic, tensor_ops, image_utils
-from . import generators, model_helper, applications
-    
-# from keras import backend as K
-# K.compat.v1.logging.set_verbosity(K.tf.compat.v1.logging.ERROR)
 
-# remove tensorflow warning
+# ignore warnings scikit-image, numpy > 1.17 in tensorflow
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
+
+from . import generic, tensor_ops, image_utils, generators, model_helper, applications
+
+# remove tensorflow warnings
 import logging
 class WarningFilter(logging.Filter):
     def filter(self, record):
@@ -17,8 +18,4 @@ class WarningFilter(logging.Filter):
 logger = logging.getLogger('tensorflow')
 logger.addFilter(WarningFilter())
 
-# if too many warnings from scikit-image 
-import warnings
-warnings.filterwarnings("ignore")
-
-print('Loaded keras utilities')
+print('Loaded KU')
