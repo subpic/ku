284dc05859085b4fcee3aab07ef2727d30b8f2d6
Oliver Wiedemann <mail@oliver-wiedemann.net>
2020-08-02 13:21:36 +0200

    fix whitespace in image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -31,13 +31,13 @@ def imv(im, remap=True, figsize=None, **kwargs):
         kwargs.setdefault('cmap','gray')
     plt.imshow(im_, **kwargs);
     plt.axis('off');
-    
-def view_stack(ims, figsize=(20, 20), figshape=None, 
+
+def view_stack(ims, figsize=(20, 20), figshape=None,
                cmap='gray', vrange='all', **kwargs):
     """
     Display a stack or list of images using subplots.
 
-    * ims: single np.ndarray of size [N x H x W x 3/1] or 
+    * ims: single np.ndarray of size [N x H x W x 3/1] or
            list of np.ndarray(s) of size [H x W x 3/1]
            (if list, np.stack is called first)
     * figsize: plt.figure(figsize=figsize)
@@ -51,8 +51,8 @@ def view_stack(ims, figsize=(20, 20), figshape=None,
     """
     # get number of images
     if isinstance(ims, list): n = len(ims)
-    else:                     n = ims.shape[0] 
-        
+    else:                     n = ims.shape[0]
+
     if figshape is None:
         cols = int(np.ceil(np.sqrt(n)))
         rows = int(np.ceil(old_div(1.*n,cols)))
@@ -61,9 +61,9 @@ def view_stack(ims, figsize=(20, 20), figshape=None,
     if vrange == 'all':
         if isinstance(ims, list):
             mm = list(map(minmax, ims))
-            vrange = (min([p[0] for p in mm]), 
+            vrange = (min([p[0] for p in mm]),
                       max([p[1] for p in mm]))
-        else: 
+        else:
             vrange = minmax(ims)
     elif vrange == 'each':
         vrange = (None, None)
@@ -72,12 +72,12 @@ def view_stack(ims, figsize=(20, 20), figshape=None,
     for i in range(n):
         ax = fig.add_subplot(rows, cols, i+1)
         if isinstance(ims, list): im = ims[i]
-        else:                     im = np.squeeze(ims[i, ...])            
-        ax.imshow(im, cmap=cmap, 
+        else:                     im = np.squeeze(ims[i, ...])
+        ax.imshow(im, cmap=cmap,
                   vmin=vrange[0], vmax=vrange[1], **kwargs)
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
-        
+
 def read_image(image_path, image_size=1):
     """
     Read image from disk
@@ -88,17 +88,17 @@ def read_image(image_path, image_size=1):
     :return: np.ndarray
     """
     if type(image_size) == tuple:
-        im = load_img(image_path, target_size=image_size) 
+        im = load_img(image_path, target_size=image_size)
         x = img_to_array(im)
     else:
         im = load_img(image_path)
-        x = img_to_array(im)            
+        x = img_to_array(im)
         if not image_size == 1:
-            new_size = list(map(int, (x.shape[0]*image_size, x.shape[1]*image_size)))        
+            new_size = list(map(int, (x.shape[0]*image_size, x.shape[1]*image_size)))
             x = transform.resize(x/255., new_size, mode='reflect')*255.
     return x
 
-def write_image(im_arr, dst_path, fmt='jpg'):    
+def write_image(im_arr, dst_path, fmt='jpg'):
     im = array_to_img(im_arr)
     make_dirs(dst_path)
     if fmt.lower() in ('jpg', 'jpeg'):
@@ -112,7 +112,7 @@ def read_image_batch(image_paths, image_size=None, as_list=False):
 
     * image_paths: list of image paths
     * image_size: if not None, image is resized
-    * as_list: if True, return list of images, 
+    * as_list: if True, return list of images,
                else return np.ndarray (default)
     :return: np.ndarray or list
     """
@@ -156,17 +156,17 @@ def glob_images(image_path, verbose=False, split=False, image_types =\
     for imtype in image_types:
         pattern = os.path.join(image_path, imtype)
         file_list.extend(glob.glob(pattern))
-                
+
     if verbose:
         print('\nFound', len(file_list), 'images')
 
     if split:
-        return list(zip(*[os.path.split(impath) for impath in file_list]))   
+        return list(zip(*[os.path.split(impath) for impath in file_list]))
     else:
         return file_list
 
 def resize_folder(path_src, path_dst, image_size_dst=None,
-                  overwrite=False, format_dst='jpg', 
+                  overwrite=False, format_dst='jpg',
                   process_fn=None, jpeg_quality=95):
     """
     Resize an image folder, copying the resized images to a new destination folder.
@@ -181,24 +181,24 @@ def resize_folder(path_src, path_dst, image_size_dst=None,
     * jpeg_quality:   quality level if saving as a JPEG image
     :return:          list of file names that triggered an error during read/resize/write
     """
-    
+
     file_list = glob_images(path_src)
     make_dirs(path_dst)
     print('Resizing images from "{}" to "{}"'.format(path_src, path_dst))
-    
+
     errors = []
     for i, file_path_src in enumerate(file_list):
         show_progress(i, len(file_list), prefix='Resizing')
 
-        try:            
+        try:
             file_name = os.path.split(file_path_src)[1]
             (file_body, file_ext) = os.path.splitext(file_name)
-            
+
             file_name_dst = file_body + '.' + format_dst.lower()
             file_path_dst = os.path.join(path_dst, file_name_dst)
 
             # check that image hasn't been already processed
-            if overwrite or not os.path.isfile(file_path_dst): 
+            if overwrite or not os.path.isfile(file_path_dst):
                 im = Image.open(file_path_src)
                 if process_fn is not None:
                     im = process_fn(im)
@@ -214,12 +214,12 @@ def resize_folder(path_src, path_dst, image_size_dst=None,
                     imx.save(file_path_dst, 'JPEG', quality=jpeg_quality)
                 else:
                     imx.save(file_path_dst, format_dst.upper())
-        
+
         except Exception as e:
             print('Error saving', file_name)
             print('Exception:', e)
             errors.append(file_name)
-            
+
     return errors
 
 def check_images(image_dir, image_types =\
@@ -231,20 +231,20 @@ def check_images(image_dir, image_types =\
     * image_types: match patterns for image file extensions, defaults:
                    ('*.jpg', '*.png', '*.bmp', '*.JPG', '*.BMP', '*.PNG')
     :return:       tuple of (list of failed image names, list of all image names)
-    """    
+    """
     file_list = glob_images(image_dir, image_types)
     image_names_err = []
     image_names_all = []
     for (i, file_path) in enumerate(file_list):
         show_progress(i, len(file_list), prefix='Checking')
-        
-        try:            
+
+        try:
             file_dir, file_name = os.path.split(file_path)
             file_body, file_ext = os.path.splitext(file_name)
             image_names_all.append(file_name)
             load_img(file_path) # try to load
         except:
-            image_names_err.append(file_name)            
+            image_names_err.append(file_name)
     return (image_names_err, image_names_all)
 
 def save_images_to_h5(image_path, h5_path, overwrite=False,
@@ -260,28 +260,28 @@ def save_images_to_h5(image_path, h5_path, overwrite=False,
     """
     file_list = glob_images(image_path, ['*.jpg'])
 #     file_list = glob.glob(os.path.join(image_path, '*.jpg'))
-#     print('Found', len(file_list), 'JPG images')  
+#     print('Found', len(file_list), 'JPG images')
     make_dirs(h5_path)
     print('Saving images from "{}" to "{}"'.format(image_path, h5_path))
-    
+
     with H5Helper(h5_path, overwrite=overwrite) as h:
         for i, batch in enumerate(chunks(file_list, batch_size)):
             if i % 10 == 0:
                 print(i*batch_size, end=' ')
             image_names = [str(os.path.basename(path)) for path in batch]
             images = read_image_batch(batch, image_size=image_size_dst)
-            h.write_data(images, dataset_names=image_names)            
+            h.write_data(images, dataset_names=image_names)
 
-            
+
 def augment_folder(path_src, path_dst, process_gen, format_dst='jpg',
                    overwrite=False, verbose=True, simulate=False, file_list=None):
     """
-    Augment an image folder, copying the augmented versions of the images to a new destination folder 
+    Augment an image folder, copying the augmented versions of the images to a new destination folder
     and returning a pd.DataFrame containing augmentation paths and parameters
 
     * path_src: source folder path
     * path_dst: destination folder path, created if does not exist
-    * process_gen: generator returning `process_fn(im)` function to use 
+    * process_gen: generator returning `process_fn(im)` function to use
                    for each augmentation and a dictionary of arguments applied
     * format_dst: format type, defaults to 'jpg'
     * overwrite: enable to over-write destination images
@@ -290,10 +290,10 @@ def augment_folder(path_src, path_dst, process_gen, format_dst='jpg',
     * file_list: list of files to be processed
     :return: ids type pd.DataFrame
     """
-    
+
     if file_list is None:
         file_list = glob_images(path_src, verbose=verbose)
-    
+
     if not simulate:
         make_dirs(path_dst)
     if verbose:
@@ -333,7 +333,7 @@ def augment_folder(path_src, path_dst, process_gen, format_dst='jpg',
                 # check if image has already been processed
                 if overwrite or not os.path.isfile(file_path_dst):
                     if isinstance(args, dict):
-                        args_ = updated_dict(args, file_name=file_name, 
+                        args_ = updated_dict(args, file_name=file_name,
                                             only_existing=False)
                         imx = array_to_img(process_fn(im, **args_))
                     else:
@@ -345,11 +345,12 @@ def augment_folder(path_src, path_dst, process_gen, format_dst='jpg',
                         imx.save(file_path_dst, 'JPEG', quality=95)
                     else:
                         imx.save(file_path_dst, format_dst.upper())
-                    
+
             row_entry = [file_name, file_name_dst] + args_values
             ids_list.append(row_entry)
-                
-            
-    ids_aug = pd.DataFrame(ids_list, 
+
+
+    ids_aug = pd.DataFrame(ids_list,
                            columns=['image_name','image_path']+args_keys)
     return ids_aug
+
